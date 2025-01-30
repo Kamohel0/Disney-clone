d69ea8d5cd4f05ab67a95907c397928b787d22a2
Kamohel0 <k_mokebe@icloud.com>
2025-01-30 16:50:01 +0200

    initial2

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,11 +21,96 @@ const Content = styled.div`
  justify-content: center;
  align-items: center;
  flex-direction: column;
+ padding: 80px 40px;
+ height: 100%;
+`;
+
+const BgImage = styled.div`
+height: 100%;
+background-position: top;
+background-size: cover;
+background-repeat: no-repeat;
+background-image: url("/images/login-background.jpg");
+position: absolute;
+top: 0%;
+right: 0%;
+left: 0%;
+z-index: -1;
+`;
+
+const CTA = styled.div`
+margin-bottom:  2vw;
+max-width: 650px;
+flex-wrap: wrap;
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin-top: 0;
+align-items: center;
+text-align: center;
+margin-right: auto;
+margin-left:auto ;
+transition-timing-function:ease-out;
+transform: opacity 0.2s;
+width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%;
+`;
+
+const SignUp = styled.a`
+
+font-weight:bold;
+color: #f9f9f9;
+background-color: #0063e5;
+margin-bottom: 12px;
+width: 100%;
+letter-spacing: 1.5px;
+font-size: 18px;
+padding: 16.5px 0;
+border:  1px solid transparent;
+border-radius: 4px;
+
+&:hover {
+    background-color: #0483ee;
+}
+`;
+
+const Description = styled.p`
+color: hsla(0, 0%, 95.3, 1);
+font-size: 11px;
+margin: 0 0 24px;
+line-height: 1.5;
+letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+max-width: 600px;
+margin-bottom: 20px;
+display: inline-block;
+vertical-align: bottom;
+width: 100%;
 `;
 const Login = (props) => {
   return (
     <Container>
-        <Content> Content</Content>
+        <Content> 
+            <CTA>
+                <CTALogoOne  src="/images/cta-logo-one.svg" alt=""/>
+                <SignUp>GET ALL THERE</SignUp>
+                <Description>Get Premier Access to Raya and the Last Dragon for an additinal fee
+                    with a Disney+ subscription. As of 03/17/25, the price of Disney+ and The Disney Bundle 
+                    will increase by R18.
+                </Description>
+                <CTALogoTwo src= "/images/cta-logo-two.png"/>
+            </CTA>
+            <BgImage />
+        </Content>
     </Container>
   );
 };
