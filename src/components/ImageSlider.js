795bcab4bb18cd6db2b424abf3936e4878b8f02d
Kamohel0 <k_mokebe@icloud.com>
2025-02-10 02:02:50 +0200

    Weekend Commit(Viewers and sliders)

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -14,15 +14,28 @@ const ImgSlider = (props)  => {
     }
     return (
         <Carousel {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-badging.jpg" alt=""/>
+                    </a>
+                </Wrap>
+
+
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-scale.jpg" alt=""/>
+                    </a>
+                </Wrap>
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-badag.jpg" alt=""/>
+                    </a>
+                </Wrap>
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-scales.jpg" alt=""/>
+                    </a>
+                </Wrap>
         </Carousel>
   );
 };
@@ -50,5 +63,50 @@ ul li button{
         color: rgb(150, 158, 171);
     }
 }
+
+li.slick-active button::before {
+    color: white;
+}
+
+.slick-list{
+    overflow:  initial;
+}
+
+.slick-prev {
+    left: -75px;
+}
+
+.slick-next {
+    right: -75px;
+}
+`;
+
+const Wrap = styled.div`
+border-radius: 4px;
+cursor: pointer;
+position: relative;
+
+
+a{
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px , 
+                rgb(0 0 0 / 73%)0px 16px  10px -10px;
+                cursor: pointer;
+                display: block;
+                position: relative;
+                pad:  4px;
+
+
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+                &:hover{
+                    padding: 0;
+                    border: 4px solid rgb(249, 249, 249, 0.8);
+                    transition-duration: 300ms;
+                }
+}
+
 `;
 export default ImgSlider;
